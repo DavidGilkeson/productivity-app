1a38b1d107f0000f21e2622dd480b8874d1974b2
David Gilkeson <davidgilkeson8@gmail.com>
2020-07-01 21:54:59 +0100

    todo list delete functionality added

--- js/tasklist.js
+++ js/tasklist.js
@@ -2,12 +2,15 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 
 // Event Listeners
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo)
 
 
 
@@ -43,4 +46,59 @@ function addTodo(event) {
   todoList.append(todoDiv);
   //Clear todoInput value
   todoInput.value = "";
+}
+
+function deleteCheck(e) {
+
+  const item = e.target;
+  //Delete Todo
+  if (item.classList[0] === "delete-btn") {
+    const todo = item.parentElement;
+    // Animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+
+      todo.remove();
+    });
+
+  }
+
+  //Check Mark
+  if (item.classList[0] === "complete-btn") {
+
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+
+    if (todo.classList !== undefined) {
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if (todo.classList.contains('completed')) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains('completed')) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+      }
+    }
+    return;
+  })
+
+
 }
